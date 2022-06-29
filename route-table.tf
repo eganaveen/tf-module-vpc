9886c2dc53e7341db438f4bfcae257217d2298d9
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 15:51:39 +0530

    route table

--- route-table.tf
+++ route-table.tf
@@ -3,6 +3,7 @@ resource "aws_route_table" "my-rt" {
   route {
     cidr_block = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+
   }
   tags = {
     Name = "${var.ENV}-rt"
