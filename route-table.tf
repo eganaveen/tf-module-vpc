3b5f069b48ffe926390f99a5f93eb385987d2dae
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 15:23:38 +0530

    route table

--- route-table.tf
+++ route-table.tf
@@ -14,3 +14,9 @@ resource "aws_route" "route" {
   destination_cidr_block    = var.VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
+
+resource "aws_route_table_association" "route-association" {
+  count = length(aws_subnet.main.*.id)
+  subnet_id      = element(aws_subnet.main.id, count.index)
+  route_table_id = aws_route_table.my-rt.id
+}
