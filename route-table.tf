9c63a3003cf0cfd482d9e1a9e46688573ab8fe4e
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 15:08:39 +0530

    route table

--- route-table.tf
+++ route-table.tf
@@ -1,11 +1,10 @@
-resource "aws_route_table" "example" {
+resource "aws_route_table" "my-rt" {
   vpc_id = aws_vpc.my-vpc-demo.id
 #  count  = length(var.SUBNET_CIDR)
   route {
-    cidr_block = var.DEFAULT_VPC_ID
+    cidr_block = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
-
   tags = {
     Name = "${var.ENV}-rt"
   }
