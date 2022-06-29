bf05c3c29890597bcd1844b62ff4aa70e11650ac
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 15:17:41 +0530

    route table

--- route-table.tf
+++ route-table.tf
@@ -1,6 +1,5 @@
 resource "aws_route_table" "my-rt" {
   vpc_id = aws_vpc.my-vpc-demo.id
-#  count  = length(var.SUBNET_CIDR)
   route {
     cidr_block = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
@@ -10,3 +9,8 @@ resource "aws_route_table" "my-rt" {
   }
 }
 
+resource "aws_route" "route" {
+  route_table_id            = var.DEFAULT_ROUTE_TABLE_ID
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,4 @@ variable "SUBNET_CIDR" {}
 variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_ROUTE_TABLE_ID" {}
