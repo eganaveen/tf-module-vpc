219b88c97c9905a16046ef34757c63517776d860
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 11:15:57 +0530

    deleted all vpc and related subnets, route table

--- vars.tf
+++ vars.tf
@@ -1,7 +1,2 @@
 variable "VPC_CIDR" {}
 variable "ENV" {}
-variable "SUBNET_CIDR" {}
-variable "AZ" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_RT" {}
--- vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.ENV}-vpc"
-  }
-}
