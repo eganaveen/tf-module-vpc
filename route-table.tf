73b0dfebe99e7dd09858864a2ac368721ac3177a
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-30 12:38:45 +0530

    route table

--- route-table.tf
+++ route-table.tf
@@ -1,22 +1,22 @@
-resource "aws_route_table" "my-rt" {
-  vpc_id = aws_vpc.my-vpc-demo.id
-  route {
-    cidr_block = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-  }
-  tags = {
-    Name = "${var.ENV}-rt"
-  }
-}
-
-resource "aws_route" "route" {
-  route_table_id            = var.DEFAULT_ROUTE_TABLE_ID
-  destination_cidr_block    = var.VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-}
-
-resource "aws_route_table_association" "route-association" {
-  count = length(aws_subnet.main.*.id)
-  subnet_id      = element(aws_subnet.main.*.id, count.index)
-  route_table_id = aws_route_table.my-rt.id
-}
+#resource "aws_route_table" "my-rt" {
+#  vpc_id = aws_vpc.my-vpc-demo.id
+#  route {
+#    cidr_block = var.DEFAULT_VPC_CIDR
+#    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#  }
+#  tags = {
+#    Name = "${var.ENV}-rt"
+#  }
+#}
+#
+#resource "aws_route" "route" {
+#  route_table_id            = var.DEFAULT_ROUTE_TABLE_ID
+#  destination_cidr_block    = var.VPC_CIDR
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#}
+#
+#resource "aws_route_table_association" "route-association" {
+#  count = length(aws_subnet.main.*.id)
+#  subnet_id      = element(aws_subnet.main.*.id, count.index)
+#  route_table_id = aws_route_table.my-rt.id
+#}
--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,20 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.my-vpc-demo.id
 
-  count = length(var.SUBNET_CIDR)
-  cidr_block = element(var.SUBNET_CIDR, count.index)
+  count = length(var.PUBLIC_SUBNET_CIDR)
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
   availability_zone = element(var.AZ, count.index )
   tags = {
-    Name = "${var.ENV}-${element(var.AZ, count.index)}"
+    Name = "${var.ENV}-pub-${element(var.AZ, count.index)}"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.my-vpc-demo.id
+
+  count = length(var.PRIVATE_SUBNET_CIDR)
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone = element(var.AZ, count.index )
+  tags = {
+    Name = "${var.ENV}-priv-${element(var.AZ, count.index)}"
   }
 }
