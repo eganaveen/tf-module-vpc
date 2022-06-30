54be24386e2938bb98e163cafb56dc6598cb84d3
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-30 10:47:45 +0530

    route table

--- igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.my-vpc-demo.id
-
-  tags = {
-    Name = "${var.ENV}-igw"
-  }
-}
--- route-table.tf
+++ route-table.tf
@@ -3,7 +3,6 @@ resource "aws_route_table" "my-rt" {
   route {
     cidr_block = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-
   }
   tags = {
     Name = "${var.ENV}-rt"
